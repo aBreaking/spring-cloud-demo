15f35a241486b6c696c2e7cf89669e4748845509
aBreaking <abreaking@163.com>
2018-08-03 16:42:36 +0800

    修改编码

--- provider/src/main/java/li/wei/springboot/provider/ProviderApplication.java
+++ provider/src/main/java/li/wei/springboot/provider/ProviderApplication.java
@@ -2,11 +2,15 @@ package li.wei.springboot.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RestController;
 
+
+/*@EnableEurekaClient*/
+
 @SpringBootApplication
-@EnableEurekaClient
+@EnableConfigServer
 public class ProviderApplication {
 
     public static void main(String[] args) {
